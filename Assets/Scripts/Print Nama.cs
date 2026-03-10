07f5c1307f75ed749c4f9e31f442269740130db6
Bhumm579 <bhumiaqiel1@gmail.com>
2026-03-10 10:53:50 +0700

    Menambahkan nama Bhumi

--- Assets/Scripts/Print Nama.cs
+++ Assets/Scripts/Print Nama.cs
@@ -5,7 +5,7 @@ public class PrintNama : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Debug.Log("Nama Saya Adalah : Bhumi");
+        Debug.Log("Nama Saya Adalah : Ahay");
     }
 
     // Update is called once per frame
