91182301439b15e16e27385c9725911336a4262c
Bhumm579 <bhumiaqiel1@gmail.com>
2026-03-10 10:36:24 +0700

    Menambahkan debug log bhumi

---
+++ Assets/Scripts/Print Nama.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PrintNama : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Nama Saya Adalah : Bhumi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
